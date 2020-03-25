fc0518d3aa71eb448769277fafe41aee637183cb
Phung Thanh Hai <phungthanhhai58@gmail.com>
2020-03-25 17:06:59 +0700

    complete configuration

---
+++ eShopSolution.Data/Configurations/LanguageConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using eShopSolution.Data.entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.Configurations
+{
+    public class LanguageConfiguration : IEntityTypeConfiguration<Contact>
+    {
+        public void Configure(EntityTypeBuilder<Contact> builder)
+        {
+            builder.ToTable("Languages");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id).IsRequired().IsUnicode(false).HasMaxLength(5);
+
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(20);
+        }
+    }
+}
